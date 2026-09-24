0712a37ec37cfcf90f10b0cdb66fed94934516ee
Skrooge Deploy <deploy@skrooge.ai>
2026-09-24 17:04:39 +0200

    Deploy mainacc-september demo (manager Main + BP General)

--- assets/SectionCard.vue_vue_type_style_index_1_lang.DQEbyBHw.js
+++
@@ -1,1 +0,0 @@
-import{aT as e}from"./index.BYRSqZM8.js";function t(){const o=e();return{schedule:o.schedule,cancel:o.cancel,commitNow:o.commitNow,flushAll:o.flushAll}}export{t as u};
---
+++ assets/SectionCard.vue_vue_type_style_index_1_lang.DvNcJpK3.js
@@ -0,0 +1,1 @@
+import{aT as e}from"./index.DcVCyURG.js";function t(){const o=e();return{schedule:o.schedule,cancel:o.cancel,commitNow:o.commitNow,flushAll:o.flushAll}}export{t as u};
